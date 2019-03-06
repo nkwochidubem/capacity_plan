3222aebf2b512d060407972ed287a793bde2fc82
Nkwo Chidubem Ambrose <nkwochidubem@users.noreply.github.com>
2019-03-06 16:47:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
 from setuptools import setup, find_packages
+import os
+
 import re, ast
 
-# get version from __version__ variable in bench/__init__.py
+# get version from __version__ variable in app's/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-with open('bench/__init__.py', 'rb') as f:
+with open('capacity_plan/__init__.py', 'rb') as f:
 	version = str(ast.literal_eval(_version_re.search(
 		f.read().decode('utf-8')).group(1)))
 
 setup(
-	name='bench',
-	description='Metadata driven, full-stack web framework',
-	author='Chidubem ambrose',
-	author_email='chidubem.ambrose@softclo.com',
-	version=version,
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires,
-	entry_points='''
-[console_scripts]
-bench=bench.cli:cli
-''',
+    name='capacity_plan',
+    version=version,
+    description='Capacity Plan',
+    author='Chidubem',
+    author_email='chidubem.ambrose@softclo.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=("frappe",),
 )
